b5d356bf6476c96b12651bd6ca009002c774a424
Ryusichan <Ryusichan@github.com>
2021-11-30 18:45:49 +0900

    icon 정리

--- src/layouts/dashboard/SidebarConfig.tsx
+++ src/layouts/dashboard/SidebarConfig.tsx
@@ -1,6 +1,14 @@
 import { Icon } from "@iconify/react";
-import pieChart2Fill from "@iconify/icons-eva/pie-chart-2-fill";
-import peopleFill from "@iconify/icons-eva/people-fill";
+import applicationSettings from "@iconify/icons-mdi/application-settings-outline";
+import clockOutline from "@iconify/icons-mdi/clock-outline";
+import accountSettingsSutline from "@iconify/icons-mdi/account-settings-outline";
+import weatherWindy from "@iconify/icons-mdi/weather-windy";
+import clipboardClock from "@iconify/icons-mdi/clipboard-text-clock-outline";
+import textSearch from "@iconify/icons-mdi/text-search";
+import bellOutline from "@iconify/icons-mdi/bell-outline";
+import noteText from "@iconify/icons-mdi/note-text-outline";
+import layers from "@iconify/icons-mdi/layers";
+import dishwasher from "@iconify/icons-mdi/dishwasher";
 import shoppingBagFill from "@iconify/icons-eva/shopping-bag-fill";
 import fileTextFill from "@iconify/icons-eva/file-text-fill";
 import lockFill from "@iconify/icons-eva/lock-fill";
@@ -14,43 +22,43 @@ const sidebarConfig = [
   {
     title: "자동운전화면",
     path: "/dashboard/app",
-    icon: getIcon(pieChart2Fill),
+    icon: getIcon(applicationSettings),
   },
   {
     title: "누적시간화면",
     path: "/dashboard/accumulatedtime",
-    icon: getIcon(peopleFill),
+    icon: getIcon(clockOutline),
   },
   //menual
   {
     title: "수동운전화면",
     path: "/dashboard/MenualLayout",
-    icon: getIcon(shoppingBagFill),
+    icon: getIcon(accountSettingsSutline),
   },
   {
     title: "고액분리기 설정화면",
     path: "/dashboard/HighSeparator",
-    icon: getIcon(fileTextFill),
+    icon: getIcon(layers),
   },
   {
     title: "시간세척 설정화면",
     path: "/dashboard/TimeWashing",
-    icon: getIcon(shoppingBagFill),
+    icon: getIcon(dishwasher),
   },
   {
     title: "발효건조기 설정화면",
     path: "/dashboard/FermentedDryer",
-    icon: getIcon(shoppingBagFill),
+    icon: getIcon(weatherWindy),
   },
   {
     title: "작동/알람 기록화면",
     path: "/dashboard/NotificationRecord",
-    icon: getIcon(lockFill),
+    icon: getIcon(clipboardClock),
   },
   {
     title: "적산전력 기록화면",
     path: "/dashboard/IntegratedPower",
-    icon: getIcon(lockFill),
+    icon: getIcon(textSearch),
   },
   //manager
   // {
@@ -61,12 +69,12 @@ const sidebarConfig = [
   {
     title: "알림",
     path: "/dashboard/ManagerNoti",
-    icon: getIcon(lockFill),
+    icon: getIcon(bellOutline),
   },
   {
     title: "기록",
     path: "/dashboard/ManagerHistory",
-    icon: getIcon(lockFill),
+    icon: getIcon(noteText),
   },
   //global
   // {
