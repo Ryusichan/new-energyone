adfb8348646df3e995314e5f92ae3d99ffde0916
Ryusichan <Ryusichan@github.com>
2021-12-06 19:03:47 +0900

    적산전력 기록화면 push

---
+++ src/components/_dashboard/integratedPower/InterValue.tsx
@@ -0,0 +1,108 @@
+import {
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Grid,
+} from "@mui/material";
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import { inteValue } from "../../../_mocks_/integratedPower";
+
+const innerValueStyle = {
+  fontSize: 20,
+  backgroundColor: "#2196f3",
+  padding: "2px 6px",
+  borderRadius: "4px",
+  color: "#fff",
+};
+
+const GridBox = styled(Grid)(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  padding: theme.spacing(3),
+  borderRadius: 12,
+}));
+
+const InterValue = () => {
+  return (
+    <GridBox item xs={12}>
+      <Typography variant="h6">시간세척 설정</Typography>
+      <List>
+        {inteValue.map((data) => (
+          <DetailSetting name={data.name} value={data.value} />
+        ))}
+      </List>
+    </GridBox>
+  );
+};
+
+interface Props {
+  name: string;
+  value: number;
+}
+
+const DetailSetting = ({ name, value }: Props) => {
+  const [detailValue, setValue] = useState<number>(value);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    setOpenModal(false);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleOpenModal();
+    }
+  };
+  return (
+    <>
+      <ListItem>
+        <ListItemText primary={name} />
+        <TextField
+          size="small"
+          sx={{ maxWidth: 54, marginRight: 1 }}
+          value={detailValue}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+          type="number"
+        />
+        %
+      </ListItem>
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle id="modal-modal-title">{name}</DialogTitle>
+        <DialogContent id="modal-modal-description" sx={{ mt: 2 }}>
+          {name}을 <span style={innerValueStyle}>{detailValue}%</span>로 설정
+          하시겠습니까?
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} color={"inherit"}>
+            취소
+          </Button>
+          <Button onClick={handleSubmit} variant={"contained"}>
+            설정
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default InterValue;
--- src/pages/menual/IntegratedPower.tsx
+++ src/pages/menual/IntegratedPower.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+// material
+import {
+  Box,
+  Grid,
+  Container,
+  Typography,
+  Button,
+  TextField,
+  IconButton,
+} from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+
+// components
+import InterPower from "../../components/_dashboard/integratedPower/InterPower";
+import IntermiddleVoltage from "../../components/_dashboard/integratedPower/IntermiddleVoltage";
+import InterVoltage from "../../components/_dashboard/integratedPower/InterVoltage";
+import InterValue from "../../components/_dashboard/integratedPower/InterValue";
+
+const GridBox = styled(Grid)(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  padding: theme.spacing(3),
+  borderRadius: 12,
+}));
 
 const IntegratedPower = () => {
-  return <div>IntegratedPower</div>;
+  return (
+    <div title="IntegratedPower">
+      <Container>
+        <Box>
+          <Typography variant="h4" sx={{ mb: 5 }}>
+            적산전력 기록화면
+          </Typography>
+        </Box>
+        <Grid container spacing={4}>
+          <Grid item container xs={12} sm={6} md={6}>
+            <InterPower />
+          </Grid>
+          <Grid item container xs={12} sm={6} md={6}>
+            <IntermiddleVoltage />
+          </Grid>
+
+          <Grid item container xs={12} sm={6} md={6}>
+            <InterVoltage />
+          </Grid>
+          <Grid item container xs={12} sm={6} md={6}>
+            <InterValue />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 };
 
 export default IntegratedPower;
